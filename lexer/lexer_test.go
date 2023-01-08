5bd543da96fb90104032e27344cc7ded161efaff
chillout2san <chillout2san@gmail.com>
2023-01-09 00:59:31 +0900

    feat: ホワイトスペースを処理したい

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -59,15 +59,15 @@ func TestGetToken(t *testing.T) {
 	`
 	lexer := NewLexer(input)
 
-	for _, ts := range tokenSet {
+	for i, ts := range tokenSet {
 		token := lexer.GetToken()
 
-		if token.Type == ts.expectedType {
-			t.Errorf("expected: %v, got: %v", ts.expectedType, token.Type)
+		if token.Type != ts.expectedType {
+			t.Errorf("index: %d, expected: %v, got: %v", i, ts.expectedType, token.Type)
 		}
 
-		if token.Literal == ts.expectedLiteral {
-			t.Errorf("expected: %v, got: %v", ts.expectedLiteral, token.Literal)
+		if token.Literal != ts.expectedLiteral {
+			t.Errorf("index: %d, expected: %v, got: %v", i, ts.expectedLiteral, token.Literal)
 		}
 		lexer.ReadChar()
 	}
